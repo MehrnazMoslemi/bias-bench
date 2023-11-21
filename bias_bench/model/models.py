831bfaa9eef8f6a7c62b9790e17a92a0713ab26d
Mehrnaz Moslemi <99104169+MehrnazMoslemi@users.noreply.github.com>
2023-11-21 15:37:55 -0500

    Update models.py

--- bias_bench/model/models.py
+++ bias_bench/model/models.py
@@ -17,9 +17,9 @@ class AlbertModel:
         return transformers.AlbertModel.from_pretrained(model_name_or_path)
 
 
-#class RobertaModel:
-#    def __new__(self, model_name_or_path):
-        
+ class RobertaModel:
+    def __new__(self, model_name_or_path):
+        return transformers.RobertaModel.from_pretrained(model_name_or_path)
 
 
 class GPT2Model:
